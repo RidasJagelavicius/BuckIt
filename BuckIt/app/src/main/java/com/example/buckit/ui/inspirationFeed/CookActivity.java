3120ab8f26d00ba29c32c52505c802316af42b49
sitajothi <slj2@illinois.edu>
2020-11-19 01:47:26 -0600

    kindness + cooking trending categories and lists have been added

--- BuckIt/app/src/main/java/com/example/buckit/ui/inspirationFeed/CookActivity.java
+++ BuckIt/app/src/main/java/com/example/buckit/ui/inspirationFeed/CookActivity.java
@@ -1,6 +1,40 @@
 package com.example.buckit.ui.inspirationFeed;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CookActivity extends AppCompatActivity {
+import com.example.buckit.R;
+
+public class CookActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button choco_cookie;
+    private Button fried_rice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cook_layout);
+
+        choco_cookie = (Button) findViewById(R.id.cook1_list);
+        choco_cookie.setOnClickListener(this);
+
+        fried_rice = (Button) findViewById(R.id.cook2_list);
+        fried_rice.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.cook1_list){
+            Intent intent = new Intent(this, Cook1List.class);
+            startActivity(intent);
+        } else if (v.getId() == R.id.cook2_list){
+            Intent intent = new Intent(this, Cook2List.class);
+            startActivity(intent);
+        }
+    }
+
 }
